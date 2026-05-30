83f25d2ea0128c56a27f547217a527fa3be29e8b
Luis Corea <icoreanuez@gmail.com>
2026-05-30 13:44:07 -0600

    Feature: Snowfall effect from site

---
+++ src/components/snowfall/index.jsx
@@ -0,0 +1,16 @@
+import Snowfall from "react-snowfall";
+
+export default function SnowfallEffect(props) {
+    
+    // return <Snowfall {...props}/>;
+    return <Snowfall color="#82C3D9"/>;
+}
+
+// export default function SnowfallEffect(props) {
+//     const now = new Date();
+//     const isDecember = now.getMonth()===11;
+//     if (!isDecember) {
+//         return null;        
+//     }
+//     return <Snowfall {...props}/>;
+// }
